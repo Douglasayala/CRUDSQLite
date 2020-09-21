019e0e208a729a255e8d32c4345e504e4af81815
Douglasayala <douglasayala612@gmail.com>
2020-09-20 18:52:37 -0600

    He creado iconos y la vista

---
+++ app/src/main/java/com/example/crudsql_lite/detalle_articulo.java
@@ -0,0 +1,14 @@
+package com.example.crudsql_lite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalle_articulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_articulo);
+    }
+}
